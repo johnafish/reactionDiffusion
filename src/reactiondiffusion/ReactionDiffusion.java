da031a8d80ef2772693db4dd7cac16d6f722bcc0
John Fish <john@johnafish.ca>
2015-10-05 12:18:56 -0400

    :smile_cat: Basic Reaction Diffusion
    It now does reaction diffusion with crappy graphics. As far as I can
    tell it actually works!

--- src/reactiondiffusion/ReactionDiffusion.java
+++ src/reactiondiffusion/ReactionDiffusion.java
@@ -9,24 +9,75 @@ import javax.swing.JFrame;
  * @author John Fish <john@johnafish.ca>
  */
 public class ReactionDiffusion extends JFrame {
-    int size = 100;
-    public static int windowSize = 500;
+    int size = 200;
+    public static int windowSize = 400;
     double[][] a = new double[size][size];
     double[][] b = new double[size][size];
+    double dA = 1.0;
+    double dB = 0.5;
+    double f = 0.0545;
+    double k = 0.062;
+    double delT = 1.0;
     
     public void populateInitialArray(){
+        int lowerSeed = size/2-10;
+        int upperSeed = size/2+10;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                a[i][j] = ThreadLocalRandom.current().nextDouble();
+                
+                if (i>lowerSeed && j>lowerSeed && i<upperSeed && j<upperSeed){
+                    a[i][j] = 1;
+                    b[i][j] = 1;
+                } else {
+                    a[i][j] = 1;
+                    b[i][j] = 0;
+                }
             }
         }
     }
-    public void nextGeneration(){
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                a[i][j] = ThreadLocalRandom.current().nextDouble();
+    
+    public double applyLaplacian(int x, int y, int aOrB){ //0==a, 1==b
+        //double[][] laplacian = {{0, 0, 0}, {0,1,0}, {0, 0, 0}};
+        double[][] laplacian = {{0.05, 0.2, 0.05}, {0.2,-1,0.2}, {0.05, 0.2, 0.05}};
+        double sum = 0;
+        if(aOrB==0){
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int xPos = x+(i-1);
+                    int yPos = y+(j-1);
+                    if (0<=xPos && xPos<size && 0<=yPos && yPos<size){
+                        sum += laplacian[i][j]*a[xPos][yPos];
+                    }
+                }
+            }
+        }else {
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int xPos = x+(i-1);
+                    int yPos = y+(j-1);
+                    if (0<=xPos && xPos<size && 0<=yPos && yPos<size){
+                        sum += laplacian[i][j]*b[xPos][yPos];
+                    }
+                }
             }
         }
+        return sum;
+    }
+    
+    public void nextGeneration(){
+        double[][] aN = new double[size][size];
+        double[][] bN = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                double aP = a[i][j];
+                double bP = b[i][j];
+                double reaction = aP*bP*bP;
+                aN[i][j] = aP+delT*(dA*applyLaplacian(i,j,0)-reaction+f*(1-aP));
+                bN[i][j] = bP+delT*(dB*applyLaplacian(i,j,1)+reaction-bP*(k+f));
+            }
+        }
+        a = aN;
+        b = bN;
     }
     @Override
     public void paint(Graphics g){
@@ -34,14 +85,14 @@ public class ReactionDiffusion extends JFrame {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 //bwValue is the colour value of our pixel, pure a is white pure b is black
-                int bwValue = (int) Math.round(128+a[i][j]*127-b[i][j]*127);
+                int bwValue = (int) Math.round(127 + 127*a[i][j] - 127*b[i][j]);
                 Color bw = new Color(bwValue,bwValue,bwValue);
                 g.setColor(bw);
                 g.fillRect(i*cellSize, j*cellSize, cellSize, cellSize);
             }
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ReactionDiffusion r = new ReactionDiffusion();
         r.setSize(windowSize, windowSize);
         r.setDefaultCloseOperation( EXIT_ON_CLOSE );
@@ -49,8 +100,9 @@ public class ReactionDiffusion extends JFrame {
         r.setVisible(true);  //Calls paint
         while(true){
             r.nextGeneration();
-            r.repaint();
+            r.repaint(); 
         }
+        
     }
 
 }
