0328fe20e9f3bbf66a9425e8a931e5bc4ff8410e
John Fish <john@johnafish.ca>
2015-10-05 09:20:45 -0400

    :bowtie: Added method for random initialization
    Will now initialize a and b concentrations randomly

--- src/reactiondiffusion/ReactionDiffusion.java
+++ src/reactiondiffusion/ReactionDiffusion.java
@@ -2,13 +2,25 @@ package reactiondiffusion;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.JFrame;
 
 /**
  * @author John Fish <john@johnafish.ca>
  */
 public class ReactionDiffusion extends JFrame {
-
+    int size = 30;
+    double[][] a = new double[size][size];
+    double[][] b = new double[size][size];
+    
+    public void populateInitialArray(){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                a[i][j] = ThreadLocalRandom.current().nextDouble();
+            }
+        }
+    }
+    
     @Override
     public void paint(Graphics g){
         //Drawing code goes here
@@ -17,7 +29,8 @@ public class ReactionDiffusion extends JFrame {
         ReactionDiffusion r = new ReactionDiffusion();
         r.setSize(500, 500);
         r.setDefaultCloseOperation( EXIT_ON_CLOSE );
-        r.setVisible(true); 
+        r.populateInitialArray();
+        r.setVisible(true);  //Calls paint
     }
 
 }
