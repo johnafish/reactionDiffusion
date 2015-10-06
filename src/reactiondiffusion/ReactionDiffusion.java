05dbc561b00e198bc72e392117c7a1300c4d31cc
John Fish <john@johnafish.ca>
2015-10-06 09:44:29 -0400

    Working on mousedrag

--- src/reactiondiffusion/ReactionDiffusion.java
+++ src/reactiondiffusion/ReactionDiffusion.java
@@ -2,6 +2,9 @@ package reactiondiffusion;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.ThreadLocalRandom;
 import javax.swing.JFrame;
@@ -9,19 +12,25 @@ import javax.swing.JFrame;
 /**
  * @author John Fish <john@johnafish.ca>
  */
-public class ReactionDiffusion extends JFrame {
-    public static int width = 1366;
-    public static int height = 768;
+public class ReactionDiffusion extends JFrame implements MouseListener, MouseMotionListener {
+    public static int width = 400;
+    public static int height = 400;
     double[][] a = new double[width][height];
     double[][] b = new double[width][height];
     double dA = 1.0;
     double dB = 0.5;
-    //Cool (f,k): (.018, .051)
+    //Cool (f,k): (.018, .051), 
     double f = .018; 
     double k = .051;
     double delT = 1.0;
     int timeCount = 0;
     int frameSkip = 1;
+    
+    public ReactionDiffusion(){
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+    
     public void populateInitialArray(){
         int radius = 12;
         int midX = width/2;
@@ -96,6 +105,18 @@ public class ReactionDiffusion extends JFrame {
         a = aN;
         b = bN;
     }
+    public void mousePressed( MouseEvent e ) {}
+    public void mouseClicked( MouseEvent e ) {}
+    public void mouseMoved( MouseEvent e ) { }
+    public void mouseEntered( MouseEvent e) {}
+    public void mouseExited( MouseEvent e) {}
+    public void mouseReleased( MouseEvent e) {}
+    public void mouseDragged( MouseEvent e) {
+        int mouseX = e.getX();
+        int mouseY = e.getY();
+        a[mouseX][mouseY] = .5;
+        b[mouseX][mouseY] = .5;
+    }
     @Override
     public void paint(Graphics g){
         timeCount++;
@@ -106,7 +127,7 @@ public class ReactionDiffusion extends JFrame {
                 for (int j = 0; j < height; j++) {
                     //bwValue is the colour value of our pixel, pure a is white pure b is black
                     int bwValue = (int) Math.round(128+127*a[i][j] - 127*b[i][j]);
-                    Color bw = new Color((int)Math.round(55*a[i][j]),(int)Math.round(55*b[i][j]),0);
+                    Color bw = new Color((int)Math.abs(Math.round(55*a[i][j])),(int)Math.abs(Math.round(120*b[i][j])),0);
                     img.setRGB(i, j, bw.getRGB());
                 }
             }
@@ -119,7 +140,7 @@ public class ReactionDiffusion extends JFrame {
         r.setSize(width, height);
         r.setDefaultCloseOperation( EXIT_ON_CLOSE );
         r.populateInitialArray();
-        r.setUndecorated(true);
+//        r.setUndecorated(true);
         r.setVisible(true);  //Calls paint
         while(true){
             r.nextGeneration();
